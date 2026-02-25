44f768dfdcd1ba6e6402b21f29c525a38b46fd00
vDeee092 <vd.puroy@gmail.com>
2026-02-24 18:54:33 -0800

    Added create and update feature for user ratings, reviews, and favorite

--- backend/controllers/ratingController.js
+++ backend/controllers/ratingController.js
@@ -1,7 +1,32 @@
-exports.addReview = async (req, res) => {
-  try {
-    res.send("hello");
-  } catch (error){
-    res.send(error);
-  }
+const db = require('../config/database.js')
+
+// adds or modifies a rating/review/favorite for a given user, dining hall, and food item
+exports.modifyRating = async (req, res) => {
+    try {
+        const ratingsRef = await db.collection('dining_halls').doc(req.body.diningHallID).collection('Menu').doc(req.body.foodID).collection('ratings').doc(req.userID);
+        const ratingsSnapshot = await ratingsRef.get();
+
+        updateData = {};
+        if (req.body.rating !== undefined) updateData.rating = req.body.rating;
+        if (req.body.review !== undefined) updateData.review = req.body.review;
+        if(req.body.marked_fav !== undefined) updateData.marked_fav = req.body.marked_fav;
+        if (Object.keys(updateData).length === 0) {
+            res.send("No fields to update");
+            return;
+        }
+
+        if (ratingsSnapshot.exists) {
+            ratingsRef.update(updateData);
+            res.send("Rating/review updated successfully");
+        } else {
+            ratingsRef.set(updateData);
+            res.send("Rating/review added successfully");
+        }
+    } catch (error){
+        res.send(error);
+    }
 }
+
+// TODO: add function to remove rating
+
+exports.remove
--- backend/routes/ratingRoutes.js
+++ backend/routes/ratingRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const controller = require('../controllers/ratingController')
-const {authMiddleware} = require('../middleware/authMiddleware.js')
+const controller = require('../controllers/ratingController.js')
+const {authMiddleware} = require('../middleware/authMiddleware.js');
 
-//router.use(authMiddleware);
-// add review
-router.post('/add_review', authMiddleware, controller.addReview);
+router.post('/add_review', authMiddleware, controller.modifyRating);
 
 module.exports = router;
