a66ede421221271ca7c81d98c210be4009f94910
vDeee092 <vd.puroy@gmail.com>
2026-02-28 21:51:13 -0800

    Refactor ratings to top-level collection and separate favorite feature to toggle functionality

--- backend/controllers/ratingController.js
+++ backend/controllers/ratingController.js
@@ -1,44 +1,113 @@
 const db = require('../config/database.js')
 const admin = require('../config/database.js').admin
 
-// adds or modifies a rating/review/favorite for a given user, dining hall, and food item
+// adds or modifies a user's rating/review/favorite for a given dining hall, and food item
 exports.modifyRating = async (req, res) => {
     try {
-        const ratingsRef = await db.collection('dining_halls').doc(req.body.diningHallID).collection('Menu').doc(req.body.foodID).collection('ratings').doc(req.userID);
-        const ratingsSnapshot = await ratingsRef.get();
-
-        updateData = {};
+        const ratingID = `${req.body.diningHallID}-${req.body.foodID}-${req.userID}`;
+        const ratingsRef = db.collection('ratings').doc(ratingID);
+        const updateData = {};
         if (req.body.rating !== undefined) updateData.rating = req.body.rating;
         if (req.body.review !== undefined) updateData.review = req.body.review;
-        if(req.body.marked_fav !== undefined) updateData.marked_fav = req.body.marked_fav;
         if (Object.keys(updateData).length === 0) {
             res.send("No fields to update");
             return;
         }
+        
+        updateData.userID = req.userID;
+        updateData.foodID = req.body.foodID;
+        updateData.diningHallID = req.body.diningHallID;
+        await db.runTransaction(async (transaction) => {
+            const ratingsSnapshot = await transaction.get(ratingsRef);
+            if (ratingsSnapshot.exists) {
+                transaction.update(ratingsRef, updateData);
+            } else {
+                transaction.set(ratingsRef, updateData);
+            }
+        });
 
-        if (ratingsSnapshot.exists) {
-            await ratingsRef.update(updateData);
-            res.send("Rating/review updated successfully");
-        } else {
-            await ratingsRef.set(updateData);
-            res.send("Rating/review added successfully");
-        }
+        res.send("Rating added/modified successfully");
     } catch (error){
         res.send(error);
     }
 }
 
-// removes a review for a given user, dining hall, and food item (does not remove rating or favorite status)
+// marks or unmarks food as favorite
+// if user does not have ratings/reviews or not marked as fav,
+// new user rating with only marked property created
+exports.toggleFavorite = async (req, res) => {
+    try {
+        const ratingsID = `${req.body.diningHallID}-${req.body.foodID}-${req.userID}`;
+        const ratingsRef = db.collection('ratings').doc(ratingsID);
+        
+        const updateData = {};
+        updateData.userID = req.userID;
+        updateData.foodID = req.body.foodID;
+        updateData.diningHallID = req.body.diningHallID;
+
+        await db.runTransaction(async (transaction) => {
+            const ratingsSnapshot = await transaction.get(ratingsRef);
+            if (!ratingsSnapshot.exists) {
+                updateData.marked_fav = true;
+                transaction.set(ratingsRef, updateData);
+            } else {
+                const favState = ratingsSnapshot.get('marked_fav');
+                if (favState === undefined) {
+                    updateData.marked_fav = true;
+                } else {
+                    updateData.marked_fav = !favState;
+                }
+                transaction.update(ratingsRef, updateData);
+            }
+        });
+        
+        res.send("Successfully toggled favorite");
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+// removes a user's review for a given dining hall, and food item (does not remove rating or favorite status)
 exports.removeReview = async (req, res) => {
     try {
-        const reviewRef = await db.collection('dining_halls').doc(req.body.diningHallID).collection('Menu').doc(req.body.foodID).collection('ratings').doc(req.userID);
-        const reviewSnapshot = await reviewRef.get('review');
-        if (!reviewSnapshot.exists) {
-            res.send("No review to remove");
-        } else {
-            await reviewRef.update({ review: admin.firestore.FieldValue.delete() });
+        const reviewID = `${req.body.diningHallID}-${req.body.foodID}-${req.userID}`;
+        const reviewRef = db.collection('ratings').doc(reviewID);
+
+        let hasReview = false;
+        await db.runTransaction(async (transaction) => {
+            const reviewSnapshot = await transaction.get(reviewRef);
+            if(!reviewSnapshot.exists || await reviewSnapshot.get('review') === undefined) {
+                return;
+            }
+            hasReview = true;
+            transaction.update(reviewRef, { review: admin.firestore.FieldValue.delete() });
+        });
+
+        if (hasReview) {
             res.send("Review removed successfully");
+            return;
         }
+        res.send("No review to remove");
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+exports.deleteAllUserRatings = async (req, res) => {
+    try {
+        diningHalls = await db.collection('dining_halls').select().get();
+        diningHallsIDs = diningHalls.docs.map(doc => doc.id);
+
+        for (const diningHallID of diningHallsIDs) {
+            const menuItems = await db.collection('dining_halls').doc(diningHallID).collection('Menu').select().get();
+            const menuItemIDs = menuItems.docs.map(doc => doc.id);
+            for (const menuItemID of menuItemIDs) {
+                const ratings = await db.collection('dining_halls').doc(diningHallID).collection('Menu').doc(menuItemID).collection('ratings').select().get();
+                const ratingDocs = ratings.docs.map(doc => doc.ref);
+                await db.batch().delete(...ratingDocs).commit();
+            }
+        }
+        res.send("All user ratings deleted successfully");
     } catch (error) {
         res.send(error);
     }
--- backend/routes/ratingRoutes.js
+++ backend/routes/ratingRoutes.js
@@ -5,5 +5,7 @@ const {authMiddleware} = require('../middleware/authMiddleware.js');
 
 router.post('/add_review', authMiddleware, controller.modifyRating);
 router.delete('/remove_review', authMiddleware, controller.removeReview);
+router.patch('/toggle_favorite', authMiddleware, controller.toggleFavorite);
+
 
 module.exports = router;
