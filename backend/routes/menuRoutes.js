e48fdec6e96df542807c4c5a7e4422ebb592f6d6
Julian Laxamana <julianlaxamana123@gmail.com>
2026-03-10 18:53:44 -0700

    feat: added sorting query

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const controller = require('../controllers/menuController.js')
 
 router.get('/get_menu', controller.getMenu);
+router.get('/query', controller.queryMenu);
 router.patch('/average_rating', controller.getAverageRating);
 
 module.exports = router;
